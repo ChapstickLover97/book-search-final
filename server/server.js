65b3051de5a3eaf055940e27456c71024a9ac471
ChapstickLover97 <ashtonbalder@gmail.com>
2023-05-25 12:47:06 -0500

    adjusting server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 const db = require('./config/connection'); // assuming this is the path to your connection.js file
 const typeDefs = require('./schema/typeDefs');
 const resolvers = require('./resolvers/resolvers');
@@ -14,9 +15,21 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
+// Serve static files from the React app
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 const startServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
+
+  // All remaining requests return the React app, so it's important this line is last.
+  if (process.env.NODE_ENV === 'production') {
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+  }
 
   app.listen(PORT, () => {
     console.log(`Now listening on localhost:${PORT}`);
@@ -25,4 +38,3 @@ const startServer = async () => {
 };
 
 startServer();
-
